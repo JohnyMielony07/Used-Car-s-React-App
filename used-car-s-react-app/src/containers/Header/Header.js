8871fcd6b13732b363919d1d109e37138507920c
JohnyMielony07 <jan.latoch@gmail.com>
2019-04-01 19:45:42 +0200

    add header

---
+++ used-car-s-react-app/src/containers/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <p>
+                    Best used cars in Europe!
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Header;
