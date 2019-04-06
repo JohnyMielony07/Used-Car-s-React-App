ea0601550ddd3efa592e30309d71216a2ae5d168
JohnyMielony07 <jan.latoch@gmail.com>
2019-04-06 14:40:11 +0200

    install routing in project

--- used-car-s-react-app/src/App.js
+++ used-car-s-react-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './containers/Header/Header';
@@ -7,17 +9,21 @@ import Main from './containers/Main/Main';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Header />
-        </header>
-        <main>
-          <Main />
-        </main>
-        <footer>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+          <main>
+            <Route path="/" exact component={Main} />
+            {/* <Route path="/add-car" component={AddCar} /> */}
 
-        </footer>
-      </div>
+          </main>
+          <footer>
+
+          </footer>
+        </div>
+      </BrowserRouter>
     );
   }
 }
