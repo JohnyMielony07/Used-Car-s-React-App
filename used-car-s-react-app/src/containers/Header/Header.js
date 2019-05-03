c7899bae909637c60548885a0ed3e052277453a1
JohnyMielony07 <jan.latoch@gmail.com>
2019-05-03 14:25:07 +0200

    add changing link to switching between cars list and adding car

--- used-car-s-react-app/src/containers/Header/Header.js
+++ used-car-s-react-app/src/containers/Header/Header.js
@@ -2,13 +2,23 @@ import React, { Component} from 'react';
 
 class Header extends Component {
     render () {
+        console.log(window.location.pathname)
+        let link = null;
+        switch (window.location.pathname) {
+            case '/':
+                link = <a href="/add-car">Add used car!</a>
+                break;
+            case '/add-car':
+                link = <a href="/">Back to list</a>
+                break;
+        }
         return (
             <div>
                 <p>
                     Best used cars in Europe!
                 </p>
                 <div>
-                    <a href="/add-car">Add used car!</a>
+                    {link}
                 </div>
             </div>
         )
